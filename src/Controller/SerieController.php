eaf7a8121ae83c65d51ce3f436097e5748e81bbf
ArthurENI <arthur.musenton2026@campus-eni.fr>
2026-04-28 14:56:22 +0200

    fixtures et faker

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -32,31 +32,31 @@ final class SerieController extends AbstractController
     #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
     public function create(EntityManagerInterface $entityManager): Response
     {
-        $serie = new Serie();
-        $serie->setBackdrop("backdrop.png")
-            ->setDateCreated(new \DateTime())
-            ->setFirstAirDate(new \DateTime('-6 month'))
-            ->setName('The Witcher')
-            ->setGenres('Fantastique')
-            ->setLastAirDate(new \DateTime('-1 month'))
-            ->setPopularity(5000)
-            ->setPoster('poster.png')
-            ->setStatus('returning')
-            ->setTmdbId(123456)
-            ->setVote(8);
-        dump($serie);
-
-        $entityManager->persist($serie);
-        $entityManager->flush();
-
-        dump($serie);
-
-        $serie->setName("Buffy contre les vampires");
-        $entityManager->persist($serie);
-        $entityManager->flush();
-
-        $entityManager->remove($serie);
-        $entityManager->flush();
+//        $serie = new Serie();
+//        $serie->setBackdrop("backdrop.png")
+//            ->setDateCreated(new \DateTime())
+//            ->setFirstAirDate(new \DateTime('-6 month'))
+//            ->setName('The Witcher')
+//            ->setGenres('Fantastique')
+//            ->setLastAirDate(new \DateTime('-1 month'))
+//            ->setPopularity(5000)
+//            ->setPoster('poster.png')
+//            ->setStatus('returning')
+//            ->setTmdbId(123456)
+//            ->setVote(8);
+//        dump($serie);
+//
+//        $entityManager->persist($serie);
+//        $entityManager->flush();
+//
+//        dump($serie);
+//
+//        $serie->setName("Buffy contre les vampires");
+//        $entityManager->persist($serie);
+//        $entityManager->flush();
+//
+//        $entityManager->remove($serie);
+//        $entityManager->flush();
 
         //TODO créer une série avec un formulaire
         return $this->render('serie/create.html.twig');

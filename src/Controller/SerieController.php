d02ce041d7b730a9069fbddc3bb2e8a83f29fef3
ArthurENI <arthur.musenton2026@campus-eni.fr>
2026-04-30 11:17:45 +0200

    validation et upload image

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -8,6 +8,7 @@ use App\Repository\SerieRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,8 +18,8 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SerieController extends AbstractController
 {
 //    #[Route('', name: 'list')]
-    #[Route('/list/{page}', name: 'list',requirements: ['page' => '\d+'])]
-    public function list(SerieRepository $serieRepository,int $page = 1): Response
+    #[Route('/list/{page}', name: 'list', requirements: ['page' => '\d+'])]
+    public function list(SerieRepository $serieRepository, int $page = 1): Response
     {
 //        $series = $serieRepository->findAll();
 //        $series = $serieRepository->findBy(["status" => "ended"], ['name' => 'ASC']);
@@ -28,12 +29,12 @@ final class SerieController extends AbstractController
         $maxPage = ceil($nbSeries / 50);
         if ($page < 1) {
             return $this->redirectToRoute('serie_list');
-        }elseif ($page > $maxPage ){
+        } elseif ($page > $maxPage) {
             return $this->redirectToRoute('serie_list', ['page' => $maxPage]);
         }
         $series = $serieRepository->findBestSeriesWithPagination($page);
 
-        return $this->render('serie/list.html.twig',[
+        return $this->render('serie/list.html.twig', [
             'series' => $series,
             'currentPage' => $page,
             'maxPage' => $maxPage,
@@ -41,7 +42,7 @@ final class SerieController extends AbstractController
     }
 
     #[Route('/{id}', name: 'detail', requirements: ['id' => '\d+'], methods: ['GET'])]
-    public function detail(int $id,SerieRepository $serieRepository): Response
+    public function detail(int $id, SerieRepository $serieRepository): Response
     {
 //        outils pour debug
 //        dump($id);
@@ -54,64 +55,72 @@ final class SerieController extends AbstractController
             throw $this->createNotFoundException("Serie not found");
         }
 
-        return $this->render('serie/detail.html.twig',[
+        return $this->render('serie/detail.html.twig', [
             'serie' => $serie
         ]);
     }
 
     #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
-    public function create(Request $request,
-        EntityManagerInterface $entityManager): Response
+    public function create(Request                $request,
+                           EntityManagerInterface $entityManager,
+    #[Autowire('%serie_poster_dir%')] string $posterDir): Response
     {
         $serie = new Serie();
         $serieForm = $this->createForm(SerieType::class, $serie);
 
         $serieForm->handleRequest($request);
-        $serie->setDateCreated(new \DateTime());
-        if ($serieForm->isSubmitted()) {
 
+        if ($serieForm->isSubmitted() && $serieForm->isValid()) {
+            //récupération de l'image et traitement
+            $file = $serieForm->get('poster')->getData();
+            $newFileName = $serie->getName().'-'.uniqid().'.'.$file->guessExtension();
+//       ou     $file->move($this->getParameter('serie_poster_dir'), $newFileName);
+            $file->move($posterDir, $newFileName);
+            $serie->setPoster($newFileName);
+
+            $serie->setDateCreated(new \DateTime());
             $entityManager->persist($serie);
             $entityManager->flush();
-            $this->addFlash('success',$serie->getName().' has been created');
+            $this->addFlash('success', $serie->getName() . ' has been created');
+            return $this->redirectToRoute('serie_detail', ['id' => $serie->getId()]);
         }
 
-        return $this->render('serie/create.html.twig',[
+        return $this->render('serie/create.html.twig', [
             'serieForm' => $serieForm
         ]);
     }
 
     #[Route('/{id}/delete', name: 'delete', methods: ['GET'])]
-    public function delete(int $id,SerieRepository $serieRepository,EntityManagerInterface $entityManager): Response
+    public function delete(int $id, SerieRepository $serieRepository, EntityManagerInterface $entityManager): Response
     {
         $serie = $serieRepository->find($id);
         if ($serie) {
             $entityManager->remove($serie);
         }
         $entityManager->flush();
-        $this->addFlash('success',$serie->getName().'was deleted');
+        $this->addFlash('success', $serie->getName() . 'was deleted');
         return $this->redirectToRoute('serie_list');
     }
 
-    #[Route('/{id}/update', name: 'update', methods: ['GET','POST'])]
-    public function update(int $id,SerieRepository $serieRepository,Request $request,
+    #[Route('/{id}/update', name: 'update', methods: ['GET', 'POST'])]
+    public function update(int                    $id, SerieRepository $serieRepository, Request $request,
                            EntityManagerInterface $entityManager): Response
     {
         $serie = $serieRepository->find($id);
         $serieForm = $this->createForm(SerieType::class, $serie);
         $serieForm->handleRequest($request);
 
-        if ($serieForm->isSubmitted()) {
+        if ($serieForm->isSubmitted() && $serieForm->isValid()) {
             $entityManager->persist($serie);
             $entityManager->flush();
-            $this->addFlash('success',$serie->getName().' has been updated !');
-            return $this->redirectToRoute('serie_detail',['id'=>$serie->getId()]);
+            $this->addFlash('success', $serie->getName() . ' has been updated !');
+            return $this->redirectToRoute('serie_detail', ['id' => $serie->getId()]);
         }
 
-        return $this->render('serie/update.html.twig',[
+        return $this->render('serie/update.html.twig', [
             'serieForm' => $serieForm
         ]);
     }
-
 
 
 }
